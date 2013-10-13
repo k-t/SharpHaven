c3b9944625f1ff313f14a2e88e6cd05e8a6b1303
Marat Vildanov <vildanov@corepartners.ru>
2013-10-13 19:41:03 +0400

    Implement folder resource source

---
+++ MonoHaven.Resources/Sources/FolderSource.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MonoHaven.Resources
+{
+	public class FolderSource : IEnumerableResourceSource
+	{
+		private const string FileExt = "res";
+
+		private readonly string _path;
+
+		public FolderSource(string path)
+		{
+			if (!Directory.Exists(path))
+				throw new ArgumentException("Specified path doesn't refer to an existing folder");
+
+			_path = Path.GetFullPath(path);
+
+			// add directory separator to handle relative paths correctly
+			if (!path.EndsWith(Path.DirectorySeparatorChar.ToString()))
+				_path += Path.DirectorySeparatorChar;
+		}
+
+		public string Description
+		{
+			get { return string.Format("[Folder][{0}]", _path); }
+		}
+
+		public IEnumerable<string> EnumerateAll()
+		{
+			return Directory
+				.EnumerateFiles(_path, "*." + FileExt, SearchOption.AllDirectories)
+				.Select(fileName => ToResourceName(fileName));
+		}
+
+		public Resource Get(string resName)
+		{
+			var serializer = new ResourceSerializer();
+			var fileName = ToFileName(resName);
+			using (var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+				return serializer.Deserialize(fileStream);
+		}
+
+		public void Dispose()
+		{}
+
+		private string ToFileName(string resName)
+		{
+			return Path.Combine(_path, resName) + "." + FileExt;
+		}
+
+		private string ToResourceName(string fileName)
+		{
+			var baseUri = new Uri(_path);
+			var objectName = Path.GetFileNameWithoutExtension(fileName);
+			var objectPath = Path.GetDirectoryName(fileName);
+			var objectUri = new Uri(Path.Combine(objectPath, objectName));
+			return baseUri.MakeRelativeUri(objectUri).ToString();
+		}
+	}
+}
+
---
+++ MonoHaven.Tests/Resources/FolderSourceTests.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using MonoHaven.Resources;
+
+namespace MonoHaven.Tests
+{
+	[TestFixture()]
+	public class FolderSourceTests
+	{
+		private const string SamplesPath = "Samples/FolderSourceTests";
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ThrowsExceptionWhenFolderNotExists(
+			[Values("./NotExistingFolder", SamplesPath + "/logo.res")]
+			string path)
+		{
+			new FolderSource(path);
+		}
+
+		[Test]
+		public void GetWorks(
+			[ValueSource("GetTestItems")]
+			GetTestItem testItem)
+		{
+			var source = new FolderSource(SamplesPath);
+			var resource = source.Get(testItem.ResourceName);
+
+			Assert.IsNotNull(resource);
+			Assert.AreEqual(testItem.ExpectedVersion, resource.Version);
+		}
+
+		[Test]
+		public void EnumerateAllWorks()
+		{
+			var source = new FolderSource(SamplesPath);
+
+			var result = source.EnumerateAll().ToList();
+
+			Assert.AreEqual(3, result.Count);
+			Assert.IsTrue(result.Contains("logo"));
+			Assert.IsTrue(result.Contains("logo2"));
+			Assert.IsTrue(result.Contains("SubFolder/cabin-door"));
+		}
+
+		private IEnumerable<GetTestItem> GetTestItems
+		{
+			get
+			{
+				return new[] {
+					new GetTestItem { ResourceName = "logo2", ExpectedVersion = 1 },
+					new GetTestItem { ResourceName = "SubFolder/cabin-door", ExpectedVersion = 2 }
+				};
+			}
+		}
+
+		public class GetTestItem
+		{
+			public string ResourceName { get; set; }
+			public int ExpectedVersion { get; set; }
+		}
+	}
+}
+
