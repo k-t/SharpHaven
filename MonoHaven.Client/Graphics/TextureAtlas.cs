7cbbb0188229731502acf4096ae8ac5042f5ea43
Marat Vildanov <marat.vildanov@gmail.com>
2014-10-18 20:00:34 +0400

    Refactoring

---
+++ MonoHaven.Client/Graphics/TextureAtlas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace MonoHaven.Graphics
+{
+	public class TextureAtlas : IDisposable
+	{
+		private readonly Texture texture;
+		private int nx;
+		private int ny;
+		private int maxRowHeight;
+
+		public TextureAtlas(int width, int height)
+		{
+			texture = new Texture(width, height);
+		}
+
+		public TextureRegion AddImage(byte[] imageData)
+		{
+			using (var ms = new MemoryStream(imageData))
+			using (var bitmap = new Bitmap(ms))
+			{
+				if (nx + bitmap.Width > texture.Width)
+				{
+					nx = 0;
+					ny += maxRowHeight + 2;
+					maxRowHeight = 0;
+				}
+
+				int x = nx;
+				int y = ny;
+				texture.Upload(x, y, bitmap.Width, bitmap.Height, bitmap);
+
+				nx += bitmap.Width + 2;
+				if (bitmap.Height > maxRowHeight)
+					maxRowHeight = bitmap.Height;
+
+				return new TextureRegion(texture, x, y, bitmap.Width, bitmap.Height);
+			}
+		}
+
+		public void Dispose()
+		{
+			texture.Dispose();
+		}
+	}
+}
--- MonoHaven.Client/Resources/Tileset.cs
+++ MonoHaven.Client/Resources/Tileset.cs
@@ -1,7 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Linq;
 using MonoHaven.Graphics;
 using MonoHaven.Resources.Layers;
 
@@ -9,7 +6,7 @@ namespace MonoHaven.Resources
 {
 	public class Tileset
 	{
-		private readonly Texture atlas;
+		private readonly TextureAtlas atlas;
 		private readonly WeightList<TextureRegion> groundTiles;
 		private readonly WeightList<TextureRegion>[] borderTransitions;
 		private readonly WeightList<TextureRegion>[] crossTransitions;
@@ -17,7 +14,7 @@ namespace MonoHaven.Resources
 
 		public Tileset(bool hasTransitions, IEnumerable<TileData> tiles)
 		{
-			atlas = new Texture(1024, 1024);
+			atlas = new TextureAtlas(1024, 1024);
 
 			this.hasTransitions = hasTransitions;
 			this.groundTiles = new WeightList<TextureRegion>();
@@ -32,29 +29,14 @@ namespace MonoHaven.Resources
 				}
 			}
 
-			int x = 0;
-			int y = 0;
 			foreach (var tile in tiles)
 			{
-				using (var bitmap = new Bitmap(new MemoryStream(tile.ImageData)))
-				{
-					// pack tiles to atlas
-					if (x + bitmap.Width > 1024)
-					{
-						x = 0;
-						y += bitmap.Height + 2;
-					}
-					atlas.Upload(x, y, bitmap.Width, bitmap.Height, bitmap);
-					var region = new TextureRegion(atlas, x, y, bitmap.Width, bitmap.Height);
-					x += bitmap.Width + 2;
-
-					if (tile.Type == 'g')
-						groundTiles.Add(region, tile.Weight);
-					if (tile.Type == 'c' && hasTransitions)
-						crossTransitions[tile.Id - 1].Add(region, tile.Weight);
-					if (tile.Type == 'b' && hasTransitions)
-						borderTransitions[tile.Id - 1].Add(region, tile.Weight);
-				}
+				if (tile.Type == 'g')
+						groundTiles.Add(atlas.AddImage(tile.ImageData), tile.Weight);
+				if (tile.Type == 'c' && hasTransitions)
+					crossTransitions[tile.Id - 1].Add(atlas.AddImage(tile.ImageData), tile.Weight);
+				if (tile.Type == 'b' && hasTransitions)
+					borderTransitions[tile.Id - 1].Add(atlas.AddImage(tile.ImageData), tile.Weight);
 			}
 		}
 
