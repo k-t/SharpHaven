2e06a0ec0011ed6cc29ed0818df3d5acb2de241d
Marat Vildanov <marat.vildanov@gmail.com>
2014-10-31 18:08:51 +0300

    Handle exceptions during authentication

--- MonoHaven.Client/Network/AsyncAuthClient.cs
+++ MonoHaven.Client/Network/AsyncAuthClient.cs
@@ -22,17 +22,30 @@ namespace MonoHaven.Network
 		public void Authenticate(string userName, string password)
 		{
 			Task.Factory.StartNew(() => {
-				using (var authClient = new AuthClient(host, port))
+				AuthClient authClient = null;
+				try
 				{
+					authClient = new AuthClient(host, port);
 					byte[] cookie;
-					ChangeProgress("Connecting...");
-					authClient.Connect();
 					ChangeProgress("Authenticating...");
+					authClient.Connect();
 					authClient.BindUser(userName);
 					if (authClient.TryPassword(password, out cookie))
+					{
+						ChangeProgress("Connecting...");
 						Complete(cookie);
+					}
 					else
 						Complete("Username or password incorrect");
+				}
+				catch (Exception e)
+				{
+					Complete(e.Message);
+				}
+				finally
+				{
+					if (authClient != null)
+						authClient.Dispose();
 				}
 			});
 		}
