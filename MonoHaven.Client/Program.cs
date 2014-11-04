2e6262bc644a98868b4acc3e3f0be0c293c7c513
Marat Vildanov <marat.vildanov@gmail.com>
2014-11-04 15:27:14 +0300

    Include NLog package

--- MonoHaven.Client/Program.cs
+++ MonoHaven.Client/Program.cs
@@ -1,14 +1,19 @@
 using System.Drawing;
 using System.Threading;
 using System.Windows.Threading;
+using NLog;
 using MonoHaven.Resources;
 
 namespace MonoHaven
 {
 	internal class Program
 	{
+		private static Logger Log = LogManager.GetCurrentClassLogger();
+
 		public static void Main(string[] args)
 		{
+			Log.Info("Client started");
+
 			SetSynchronizationContext();
 			using (var iconImage = EmbeddedResource.GetImage("icon.png"))
 			using (var icon = Icon.FromHandle(iconImage.GetHicon()))
