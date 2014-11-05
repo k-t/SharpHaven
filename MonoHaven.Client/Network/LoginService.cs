a986e151f8b7e93215ef299dd3ea9f217ba9cd36
Marat Vildanov <marat.vildanov@gmail.com>
2014-11-05 23:50:24 +0300

    Rewrite login method without explicit synchronization

--- MonoHaven.Client/Network/LoginService.cs
+++ MonoHaven.Client/Network/LoginService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace MonoHaven.Network
@@ -15,31 +14,20 @@ namespace MonoHaven.Network
 
 		public event EventHandler<LoginStatusEventArgs> StatusChanged;
 
-		public LoginResult Login(string userName, string password)
-		{
-			return Login(userName, password, null);
-		}
-
 		public async Task<LoginResult> LoginAsync(string userName, string password)
 		{
-			var operation = AsyncOperationManager.CreateOperation(null);
-			var result = await Task<LoginResult>.Factory.StartNew(
-				() => Login(userName, password, operation));
-			return result;
-		}
-
-		private LoginResult Login(string userName, string password, AsyncOperation operation)
-		{
-			byte[] cookie;
 			try
 			{
-				ChangeStatus("Authenticating...", operation);
-				if (!Authenticate(userName, password, out cookie))
+				ChangeStatus("Authenticating...");
+
+				var cookie = await Task<byte[]>.Factory.StartNew(() => Authenticate(userName, password));
+				if (cookie == null)
 					return new LoginResult("Username or password incorrect");
 
-				ChangeStatus("Connecting...", operation);
+				ChangeStatus("Connecting...");
+
 				var connection = CreateConnection(userName, cookie);
-				connection.Open();
+				await Task.Factory.StartNew(connection.Open);
 				return new LoginResult();
 			}
 			catch (Exception e)
@@ -48,13 +36,14 @@ namespace MonoHaven.Network
 			}
 		}
 
-		private bool Authenticate(string userName, string password, out byte[] cookie)
+		private byte[] Authenticate(string userName, string password)
 		{
+			byte[] cookie;
 			using (var authClient = new AuthClient(settings.AuthHost, settings.AuthPort))
 			{
 				authClient.Connect();
 				authClient.BindUser(userName);
-				return authClient.TryPassword(password, out cookie);
+				return authClient.TryPassword(password, out cookie) ? cookie : null;
 			}
 		}
 
@@ -70,17 +59,9 @@ namespace MonoHaven.Network
 			return new GameConnection(settings);
 		}
 
-		private void ChangeStatus(string status, AsyncOperation operation)
+		private void ChangeStatus(string status)
 		{
 			var args = new LoginStatusEventArgs(status);
-			if (operation != null)
-				operation.Post(_ => OnStatusChanged(args), null);
-			else
-				OnStatusChanged(args);
-		}
-
-		private void OnStatusChanged(LoginStatusEventArgs args)
-		{
 			if (StatusChanged != null)
 				StatusChanged(this, args);
 		}
