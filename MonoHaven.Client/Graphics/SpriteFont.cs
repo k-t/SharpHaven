7942764fb8cbfe99d578dc6087cba331f25e9752
Marat Vildanov <marat.vildanov@gmail.com>
2014-11-11 05:13:09 +0300

    Font face can be reused across sprite fonts with different sizes

--- MonoHaven.Client/Graphics/SpriteFont.cs
+++ MonoHaven.Client/Graphics/SpriteFont.cs
@@ -11,23 +11,32 @@ namespace MonoHaven.Graphics
 		private readonly Face face;
 		private readonly TextureAtlas atlas;
 		private readonly Dictionary<char, Glyph> glyphs;
+		private readonly uint pixelSize;
 
-		public SpriteFont(Face face, int pixelSize)
+		public SpriteFont(Face face, uint pixelSize)
 		{
 			atlas = new TextureAtlas(512, 512);
 			glyphs = new Dictionary<char, Glyph>();
 			this.face = face;
-			this.face.SetPixelSizes((uint)pixelSize, (uint)pixelSize);
+			this.pixelSize = pixelSize;
 		}
 
 		public int Ascent
 		{
-			get { return face.Size.Metrics.Ascender >> 6; }
+			get
+			{
+				EnsureSize();
+				return face.Size.Metrics.Ascender >> 6;
+			}
 		}
 
 		public int Height
 		{
-			get { return face.Size.Metrics.Height >> 6; }
+			get
+			{
+				EnsureSize();
+				return face.Size.Metrics.Height >> 6;
+			}
 		}
 
 		public void Dispose()
@@ -48,6 +57,8 @@ namespace MonoHaven.Graphics
 
 		private Glyph LoadGlyph(char c)
 		{
+			EnsureSize();
+
 			var index = face.GetCharIndex(c);
 			face.LoadGlyph(index, LoadFlags.Default, LoadTarget.Normal);
 			
@@ -80,6 +91,11 @@ namespace MonoHaven.Graphics
 					Image = atlas.Add(glyphPixels, PixelFormat.Rgba, sz.Width, sz.Height)
 				};
 			}
+		}
+
+		private void EnsureSize()
+		{
+			face.SetPixelSizes(pixelSize, pixelSize);
 		}
 	}
 }
