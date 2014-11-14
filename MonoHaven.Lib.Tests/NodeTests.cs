36298843ee957968834dfe8a72c0c7f1e00cb7c6
Marat Vildanov <marat.vildanov@gmail.com>
2014-11-14 11:42:21 +0300

    Add implementation of the generic tree data structure

---
+++ MonoHaven.Lib.Tests/NodeTests.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace MonoHaven.Tests
+{
+	[TestFixture]
+	public class NodeTests
+	{
+		[Test]
+		public void AddChildWorks()
+		{
+			Node<int> n, n1, n2, n3;
+
+			n = new Node<int>(0);
+			n.AddChild(n1 = new Node<int>(1));
+			n.AddChild(n2 = new Node<int>(2));
+			n.AddChild(n3 = new Node<int>(3));
+
+			Assert.That(n.Children, Is.EquivalentTo(new [] { n2, n1, n3 }),
+				"Invalid children collection");
+
+			Assert.IsTrue(
+				n.Children.All(x => x.Parent == n),
+				"Parent node is not assigned");
+		}
+
+		[Test]
+		public void AddChildReturnsThis()
+		{
+			Node<int> n1, n2, n3;
+
+			n1 = new Node<int>(0);
+			n2 = new Node<int>(1);
+			n3 = n1.AddChild(n2);
+
+			Assert.AreSame(n1, n3);
+		}
+
+		[Test]
+		public void AddChildrenWorks()
+		{
+			var node = new Node<int>(0);
+			var children = GenerateNodes(3);
+
+			node.AddChildren(children);
+
+			Assert.That(node.Children, Is.EquivalentTo(children),
+				"Invalid children collection");
+
+			Assert.IsTrue(
+				node.Children.All(x => x.Parent == node),
+				"Parent node is not assigned");
+		}
+
+		[Test]
+		public void AddChildrenReturnsThis()
+		{
+			var n1 = new Node<int>(0);
+			var children = GenerateNodes(3);
+			var n2 = n1.AddChildren(children);
+
+			Assert.AreSame(n1, n2);
+		}
+
+		[Test]
+		public void RemoveChildWorks()
+		{
+			Node<int> n, n1, n2, n3, n4;
+
+			n = new Node<int>(0);
+			n.AddChild(n1 = new Node<int>(1));
+			n.AddChild(n2 = new Node<int>(2));
+			n.AddChild(n3 = new Node<int>(3));
+			n.AddChild(n4 = new Node<int>(4));
+			n2.Remove();
+
+			Assert.IsNull(n2.Parent, "Parent node is not set to null");
+
+			Assert.That(n.Children, Is.EquivalentTo(new [] { n1, n3, n4 }),
+				"Invalid children collection after removal");
+		}
+
+		[Test]
+		public void FindWorks()
+		{
+			Node<int> n, n1, n2, n3, n4;
+
+			n = new Node<int>(0);
+			n.AddChild(n1 = new Node<int>(1));
+			n.AddChild(n2 = new Node<int>(2));
+			n.AddChild(n3 = new Node<int>(3));
+			n.AddChild(n4 = new Node<int>(2));
+
+			Assert.AreEqual(n2, n.FindChild(2));
+		}
+
+		[Test]
+		public void DescendantsWorks()
+		{
+			var node = new Node<int>(0);
+			var nodes = GenerateNodes(15);
+
+			node.AddChildren(
+				nodes[0].AddChild(
+					nodes[1].AddChild(
+						nodes[2])),
+				nodes[3].AddChildren(
+					nodes[4],
+					nodes[5].AddChild(
+						nodes[6]).AddChildren(
+							nodes[7],
+							nodes[8].AddChild(
+								nodes[9])),
+					nodes[10].AddChild(
+						nodes[11]),
+					nodes[12]),
+				nodes[13].AddChild(
+					nodes[14]));
+
+			var a = node.Descendants.ToArray();
+
+			Assert.That(node.Descendants, Is.EquivalentTo(nodes),
+				"Invalid descendant collection");
+		}
+
+		#region Helpers
+
+		private static Node<int>[] GenerateNodes(int count)
+		{
+			var children = new Node<int>[count];
+			for (int i = 0; i < count; i++)
+				children[i] = new Node<int>(i + 1);
+			return children;
+		}
+
+		#endregion
+	}
+}
---
+++ MonoHaven.Lib/Node.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MonoHaven
+{
+	public class Node<T>
+	{
+		private static readonly Func<T, T, bool> comparer = EqualityComparer<T>.Default.Equals;
+
+		private readonly T value;
+
+		private Node<T> parent;
+		private Node<T> prev;
+		private Node<T> next;
+		private Node<T> firstChild;
+		private Node<T> lastChild;
+
+		public Node()
+		{
+		}
+
+		public Node(T value)
+		{
+			this.value = value;
+		}
+
+		public Node<T> Parent
+		{
+			get { return parent; }
+		}
+
+		public IEnumerable<Node<T>> Children
+		{
+			get
+			{
+				for (var child = firstChild; child != null; child = child.next)
+					yield return child;
+			}
+		}
+
+		public IEnumerable<Node<T>> Descendants
+		{
+			get
+			{
+				var stack = new Stack<Node<T>>(Children);
+				while (stack.Any())
+				{
+					var node = stack.Pop();
+					yield return node;
+					foreach (var child in node.Children)
+						stack.Push(child);
+				}
+			}
+		}
+
+		public T Value
+		{
+			get { return value; }
+		}
+
+		/// <summary>
+		/// Adds specified node as a child to this node.
+		/// </summary>
+		public Node<T> AddChild(Node<T> node)
+		{
+			node.Remove();
+
+			node.parent = this;
+
+			if (this.firstChild == null)
+				this.firstChild = node;
+
+			if (this.lastChild != null)
+			{
+				node.prev = this.lastChild;
+				this.lastChild.next = node;
+				this.lastChild = node;
+			}
+			else
+				this.lastChild = node;
+
+			return this;
+		}
+
+		/// <summary>
+		/// Adds range of children nodes to this node.
+		/// </summary>
+		public Node<T> AddChildren(IEnumerable<Node<T>> children)
+		{
+			foreach (var child in children)
+				AddChild(child);
+			return this;
+		}
+
+		/// <summary>
+		/// Adds range of children nodes to this node.
+		/// </summary>
+		public Node<T> AddChildren(params Node<T>[] children)
+		{
+			return AddChildren(children.AsEnumerable());
+		}
+
+		/// <summary>
+		/// Removes this node from the parent node.
+		/// </summary>
+		public void Remove()
+		{
+			if (parent != null)
+			{
+				if (parent.firstChild == this)
+					parent.firstChild = next;
+				if (parent.lastChild == this)
+					parent.lastChild = prev;
+			}
+			if (next != null) next.prev = prev;
+			if (prev != null) prev.next = next;
+			parent = null;
+		}
+
+		public Node<T> FindChild(T value)
+		{
+			return Children.FirstOrDefault(x => comparer(x.Value, value));
+		}
+	}
+}
