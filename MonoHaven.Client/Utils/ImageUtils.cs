468a805b30d61b706f5d111698c295dfc9742928
Marat Vildanov <marat.vildanov@gmail.com>
2014-11-17 06:20:59 +0300

    Draw layered image taking care of offset and z-order

--- MonoHaven.Client/Utils/ImageUtils.cs
+++ MonoHaven.Client/Utils/ImageUtils.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using MonoHaven.Resources;
 
 namespace MonoHaven.Utils
@@ -10,31 +11,35 @@ namespace MonoHaven.Utils
 	{
 		public static Bitmap Combine(IEnumerable<ImageData> images)
 		{
-			var bitmaps = new List<Bitmap>();
+			var bitmaps = new List<Tuple<Bitmap, Point>>();
 			try
 			{
 				int w = 0;
 				int h = 0;
-				foreach (var image in images)
+				foreach (var image in images.OrderBy(x => x.Z))
 				{
 					var bitmap = new Bitmap(new MemoryStream(image.Data));
-					w = Math.Max(w, bitmap.Width + image.OffsetX);
-					h = Math.Max(h, bitmap.Height + image.OffsetY);
-					bitmaps.Add(bitmap);
+					w = Math.Max(w, bitmap.Width + image.DrawOffset.X);
+					h = Math.Max(h, bitmap.Height + image.DrawOffset.Y);
+					bitmaps.Add(Tuple.Create(bitmap, image.DrawOffset));
 				}
 				var combined = new Bitmap(w + 1, h + 1);
 				using (var g = System.Drawing.Graphics.FromImage(combined))
 				{
 					g.Clear(Color.Transparent);
-					foreach (var bitmap in bitmaps)
-						g.DrawImage(bitmap, 0, 0, bitmap.Width, bitmap.Height);
+					foreach (var tuple in bitmaps)
+					{
+						var bitmap = tuple.Item1;
+						var offset = tuple.Item2;
+						g.DrawImage(bitmap, offset.X, offset.Y, bitmap.Width,bitmap.Height);
+					}
 				}
 				return combined;
 			}
 			finally
 			{
 				foreach (var bitmap in bitmaps)
-					bitmap.Dispose();
+					bitmap.Item1.Dispose();
 			}
 		}
 	}
