42f3bf2e970054668bb6f5772e98ea1cd53318b4
Marat Vildanov <marat.vildanov@gmail.com>
2014-11-22 11:28:13 +0300

    Add item widget

---
+++ MonoHaven.Client/UI/ItemWidget.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using C5;
+using MonoHaven.Graphics;
+using MonoHaven.Resources;
+
+namespace MonoHaven.UI
+{
+	public class ItemWidget : Widget
+	{
+		private static readonly Drawable missing;
+		private static readonly Point defaultSize = new Point(30, 30);
+
+		static ItemWidget()
+		{
+			missing = App.Instance.Resources.GetTexture("gfx/invobjs/missing");
+		}
+
+		private readonly Delayed<Resource> res;
+		private Drawable image;
+
+		public ItemWidget(Widget parent, Delayed<Resource> res) : base(parent)
+		{
+			this.res = res;
+		}
+
+		protected override void OnDraw(DrawingContext dc)
+		{
+			if (image == null && res.Value == null)
+				dc.Draw(missing, 0, 0, defaultSize.X, defaultSize.Y);
+			else
+			{
+				if (image == null)
+				{
+					image = TextureSlice.FromBitmap(res.Value.GetLayer<ImageData>().Data);
+					FixSize();
+				}
+				dc.Draw(image, 0, 0);
+			}
+			
+		}
+
+		protected override void OnDispose()
+		{
+			if (image != null)
+				image.Dispose();
+		}
+
+		private void FixSize()
+		{
+			SetSize(image.Width, image.Height);
+		}
+	}
+}
--- MonoHaven.Client/UI/Remote/ServerItemWidget.cs
+++ MonoHaven.Client/UI/Remote/ServerItemWidget.cs
@@ -1,15 +1,25 @@
-﻿namespace MonoHaven.UI.Remote
+﻿using System.Drawing;
+
+namespace MonoHaven.UI.Remote
 {
 	public class ServerItemWidget : ServerWidget
 	{
 		public static ServerWidget Create(ushort id, ServerWidget parent, object[] args)
 		{
-			var widget = new Label(parent.Widget);
-			widget.Text = "ITEM";
+			int i = 3;
+
+			var resId = (int)args[0];
+			var q = (int)args[1];
+			var off = (int)args[2] != 0 ? (Point)args[i++] : Point.Empty;
+			var tooltip = args.Length > i ? (string)args[i++] : string.Empty;
+			var num = args.Length > i ? (int)args[i] : -1;
+
+			var res = parent.Session.GetResource(resId);
+			var widget = new ItemWidget(parent.Widget, res);
 			return new ServerItemWidget(id, parent, widget);
 		}
 
-		public ServerItemWidget(ushort id, ServerWidget parent, Widget widget)
+		public ServerItemWidget(ushort id, ServerWidget parent, ItemWidget widget)
 			: base(id, parent, widget)
 		{
 		}
