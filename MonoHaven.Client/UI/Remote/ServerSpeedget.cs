e07068b599bd20f9c4e95ed28842d20fc995b647
Marat Vildanov <marat.vildanov@gmail.com>
2014-11-22 13:30:58 +0300

    Implement speedget

--- MonoHaven.Client/UI/Remote/ServerSpeedget.cs
+++ MonoHaven.Client/UI/Remote/ServerSpeedget.cs
@@ -4,14 +4,32 @@
 	{
 		public static ServerWidget Create(ushort id, ServerWidget parent, object[] args)
 		{
-			var widget = new Label(parent.Widget);
-			widget.Text = "SPEED";
+			var cur = (int)args[0];
+			var max = (int)args[1];
+
+			var widget = new Speedget(parent.Widget);
+			widget.CurrentSpeed = cur;
+			widget.MaxSpeed = max;
 			return new ServerSpeedget(id, parent, widget);
 		}
 
-		public ServerSpeedget(ushort id, ServerWidget parent, Widget widget)
+		private readonly Speedget widget;
+
+		public ServerSpeedget(ushort id, ServerWidget parent, Speedget widget)
 			: base(id, parent, widget)
 		{
+			this.widget = widget;
+			this.widget.SpeedSelected += value => SendMessage("set", value);
+		}
+
+		public override void ReceiveMessage(string message, object[] args)
+		{
+			if (message == "cur")
+				widget.CurrentSpeed = (int)args[0];
+			else if (message == "max")
+				widget.MaxSpeed = (int)args[0];
+			else
+				base.ReceiveMessage(message, args);
 		}
 	}
 }
---
+++ MonoHaven.Client/UI/Speedget.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using MonoHaven.Graphics;
+using MonoHaven.Resources;
+using MonoHaven.Utils;
+using OpenTK;
+using OpenTK.Input;
+
+namespace MonoHaven.UI
+{
+	public class Speedget : Widget
+	{
+		private static readonly Drawable[,] images;
+		private static readonly Point size;
+
+		static Speedget()
+		{
+			images = new Drawable[4, 3];
+			var names = new[] { "crawl", "walk", "run", "sprint" };
+			var vars = new[] { "dis", "off", "on" };
+			var atlas = new TextureAtlas(128, 64);
+			int w = 0;
+			for (int i = 0; i < images.GetLength(0); i++)
+			{
+				for (int j = 0; j < images.GetLength(1); j++)
+				{
+					var res = App.Instance.Resources.Get("gfx/hud/meter/rmeter/" + names[i] + "-" + vars[j]);
+					images[i, j] = atlas.Add(res.GetLayer<ImageData>().Data);
+				}
+				w += images[i, 0].Width;
+			}
+			size = new Point(w, images[0, 0].Height);
+		}
+
+		public Speedget(Widget parent) : base(parent)
+		{
+			base.SetSize(size.X, size.Y);
+		}
+
+		public event Action<int> SpeedSelected;
+
+		public int CurrentSpeed
+		{
+			get;
+			set;
+		}
+
+		public int MaxSpeed
+		{
+			get;
+			set;
+		}
+
+		protected override void OnDraw(DrawingContext dc)
+		{
+			int x = 0;
+			for (int i = 0; i < 4; i++)
+			{
+				Drawable image;
+				if (i == CurrentSpeed)
+					image = images[i, 2];
+				else if (i > MaxSpeed)
+					image = images[i, 0];
+				else
+					image = images[i, 1];
+				dc.Draw(image, x, 0);
+				x += image.Width;
+			}
+		}
+
+		protected override void OnMouseButtonDown(MouseButtonEventArgs e)
+		{
+			var p = PointToWidget(e.Position);
+			int x = 0;
+			for (int i = 0; i < 4; i++)
+			{
+				x += images[i, 0].Width;
+				if (p.X < x)
+				{
+					SpeedSelected.Raise(i);
+					break;
+				}
+			}
+		}
+
+		protected override bool OnMouseWheel(MouseWheelEventArgs e)
+		{
+			int speed = MathHelper.Clamp(CurrentSpeed + e.Delta, 0, MaxSpeed);
+			SpeedSelected.Raise(speed);
+			return true;
+		}
+	}
+}
