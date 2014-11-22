106220f79ae5ead7bc61a58ec5f0f50ded6ab4a8
Marat Vildanov <marat.vildanov@gmail.com>
2014-11-22 06:40:32 +0300

    Remove extensions that should be never ever used

--- MonoHaven.Lib/Utils/EventExtensions.cs
+++ MonoHaven.Lib/Utils/EventExtensions.cs
@@ -16,18 +16,6 @@ namespace MonoHaven.Utils
 				action(arg);
 		}
 
-		public static void Raise<T, U>(this Action<T, U> action, T arg1, U arg2)
-		{
-			if (action != null)
-				action(arg1, arg2);
-		}
-
-		public static void Raise<T1, T2, T3>(this Action<T1, T2, T3> action, T1 arg1, T2 arg2, T3 arg3)
-		{
-			if (action != null)
-				action(arg1, arg2, arg3);
-		}
-
 		public static void Raise(this EventHandler handler, object sender, EventArgs args)
 		{
 			if (handler != null)
