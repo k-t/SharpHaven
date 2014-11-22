060b2b9e9ba797ad4c024169f0f7dd2b7df3b457
Marat Vildanov <marat.vildanov@gmail.com>
2014-11-22 10:40:35 +0300

    Add inventory widget

---
+++ MonoHaven.Client/UI/InventoryWidget.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using MonoHaven.Graphics;
+
+namespace MonoHaven.UI
+{
+	public class InventoryWidget : Widget
+	{
+		private static readonly Drawable tile;
+
+		static InventoryWidget()
+		{
+			tile = App.Instance.Resources.GetTexture("gfx/hud/invsq");
+		}
+
+		private Point inventorySize;
+
+		public InventoryWidget(Widget parent) : base(parent)
+		{
+		}
+
+		protected override void OnDraw(DrawingContext dc)
+		{
+			for (int x = 0; x < inventorySize.X; x++)
+				for (int y = 0; y < inventorySize.Y; y++)
+					dc.Draw(tile, (tile.Width - 1) * x, (tile.Height - 1) * y);
+		}
+
+		public void SetInventorySize(Point size)
+		{
+			inventorySize = size;
+			int w = (tile.Width - 1) * size.X - 1;
+			int h = (tile.Height - 1) * size.Y - 1;
+			SetSize(w, h);
+		}
+	}
+}
--- MonoHaven.Client/UI/Remote/ServerInventoryWidget.cs
+++ MonoHaven.Client/UI/Remote/ServerInventoryWidget.cs
@@ -1,15 +1,18 @@
-﻿namespace MonoHaven.UI.Remote
+﻿using System.Drawing;
+
+namespace MonoHaven.UI.Remote
 {
 	public class ServerInventoryWidget : ServerWidget
 	{
 		public static ServerWidget Create(ushort id, ServerWidget parent, object[] args)
 		{
-			var widget = new Label(parent.Widget);
-			widget.Text = "INV";
+			var size = (Point)args[0];
+			var widget = new InventoryWidget(parent.Widget);
+			widget.SetInventorySize(size);
 			return new ServerInventoryWidget(id, parent, widget);
 		}
 
-		public ServerInventoryWidget(ushort id, ServerWidget parent, Widget widget)
+		public ServerInventoryWidget(ushort id, ServerWidget parent, InventoryWidget widget)
 			: base(id, parent, widget)
 		{
 		}
