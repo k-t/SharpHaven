1035d28f6d238d27105a861d17b1832abd9fd1ec
Marat Vildanov <marat.vildanov@gmail.com>
2015-03-07 20:13:54 +0300

    Implement YAML deserializer

---
+++ MonoHaven.Lib/Resources/Serialization/Yaml/YamlResource.cs
@@ -0,0 +1,13 @@
+﻿using YamlDotNet.Serialization;
+
+namespace MonoHaven.Resources.Serialization.Yaml
+{
+	internal class YamlResource
+	{
+		[YamlMember(Alias = "version")]
+		public int Version { get; set; }
+
+		[YamlMember(Alias = "layers")]
+		public object[] Layers { get; set; }
+	}
+}
---
+++ MonoHaven.Lib/Resources/Serialization/Yaml/YamlResourceSerializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using YamlDotNet.Serialization;
+
+namespace MonoHaven.Resources.Serialization.Yaml
+{
+	public class YamlResourceSerializer : IResourceSerializer
+	{
+		private readonly Deserializer deserializer;
+
+		public YamlResourceSerializer()
+		{
+			deserializer = new Deserializer();
+			
+			Register("font", typeof(FontData), new FontDataConverter());
+			Register("image", typeof(ImageData), new ImageDataConverter());
+			Register("ninepatch", typeof(NinepatchData), new NinepatchDataConverter());
+		}
+
+		public void Register(string tag, Type type, IYamlTypeConverter converter)
+		{
+			deserializer.RegisterTagMapping("!" + tag, type);
+			deserializer.RegisterTypeConverter(converter);
+		}
+
+		public Resource Deserialize(Stream inputStream)
+		{
+			using (var reader = new StreamReader(inputStream))
+			{
+				var res = deserializer.Deserialize<YamlResource>(reader);
+				return new Resource(res.Version, res.Layers);
+			}
+		}
+
+		public void Serialize(Resource res, Stream outputStream)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
